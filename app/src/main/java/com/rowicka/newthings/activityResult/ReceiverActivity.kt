98197b6839879836ffbd845c5726d4eb81262912
RowickaM <rowickam.23@gmail.com>
2021-02-25 08:24:26 +0100

    setup binding, change layouts id

--- app/src/main/java/com/rowicka/newthings/activityResult/ReceiverActivity.kt
+++ app/src/main/java/com/rowicka/newthings/activityResult/ReceiverActivity.kt
@@ -2,11 +2,19 @@ package com.rowicka.newthings.activityResult
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.rowicka.newthings.R
+import com.rowicka.newthings.databinding.ActivityReceiverBinding
+import com.rowicka.newthings.utils.invisible
 
 class ReceiverActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityReceiverBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_receiver)
+
+        binding = ActivityReceiverBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.receiverHeader.invisible()
     }
 }
--- app/src/main/java/com/rowicka/newthings/activityResult/SenderActivity.kt
+++ app/src/main/java/com/rowicka/newthings/activityResult/SenderActivity.kt
@@ -2,11 +2,16 @@ package com.rowicka.newthings.activityResult
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.rowicka.newthings.R
+import com.rowicka.newthings.databinding.ActivitySenderBinding
 
 class SenderActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySenderBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sender)
+
+        binding = ActivitySenderBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
