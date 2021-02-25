36768d144e8290adfd0d581fbc9c88590a334347
RowickaM <rowickam.23@gmail.com>
2021-02-25 08:19:10 +0100

    prepare layout

---
+++ app/src/main/java/com/rowicka/newthings/activityResult/Constants.kt
@@ -0,0 +1,2 @@
+package com.rowicka.newthings.activityResult
+
---
+++ app/src/main/java/com/rowicka/newthings/activityResult/ReceiverActivity.kt
@@ -0,0 +1,12 @@
+package com.rowicka.newthings.activityResult
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rowicka.newthings.R
+
+class ReceiverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receiver)
+    }
+}
