fca33a4270fa78bd83cc3eb65e04724283306d02
RowickaM <rowickam.23@gmail.com>
2021-03-02 11:31:31 +0100

    improve styles

---
+++ app/src/main/java/com/rowicka/newthings/drawOnCanvas/DrawCanvasActivity.kt
@@ -0,0 +1,12 @@
+package com.rowicka.newthings.drawOnCanvas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rowicka.newthings.R
+
+class DrawCanvasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_draw_canvas)
+    }
+}
