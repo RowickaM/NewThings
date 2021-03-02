613850f5a031edf8394b5931e85ea02fafea9df6
RowickaM <rowickam.23@gmail.com>
2021-03-02 11:33:24 +0100

    prepare activity

--- app/src/main/java/com/rowicka/newthings/drawOnCanvas/DrawCanvasActivity.kt
+++ app/src/main/java/com/rowicka/newthings/drawOnCanvas/DrawCanvasActivity.kt
@@ -3,10 +3,16 @@ package com.rowicka.newthings.drawOnCanvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rowicka.newthings.R
+import com.rowicka.newthings.databinding.ActivityDrawCanvasBinding
 
 class DrawCanvasActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDrawCanvasBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_draw_canvas)
+
+        binding = ActivityDrawCanvasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
