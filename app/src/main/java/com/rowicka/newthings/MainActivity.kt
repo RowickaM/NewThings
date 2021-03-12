8dd215e4f27a06c1a42fc0137bdb5466b87991c1
RowickaM <rowickam.23@gmail.com>
2021-03-12 14:21:40 +0100

    prepare todo app

--- app/src/main/java/com/rowicka/newthings/MainActivity.kt
+++ app/src/main/java/com/rowicka/newthings/MainActivity.kt
@@ -5,6 +5,7 @@ import com.rowicka.newthings.activityResult.HomeScreenActivity
 import com.rowicka.newthings.customView.CustomViewActivity
 import com.rowicka.newthings.databinding.ActivityMainBinding
 import com.rowicka.newthings.drawOnCanvas.DrawCanvasActivity
+import com.rowicka.newthings.todoapp.tasks.TasksActivity
 import com.rowicka.newthings.utils.BaseActivity
 
 class MainActivity : BaseActivity() {
@@ -25,6 +26,7 @@ class MainActivity : BaseActivity() {
             navActivityResult.setOnClickListener { navigateToActivity(HomeScreenActivity::class) }
             navCustomView.setOnClickListener { navigateToActivity(CustomViewActivity::class) }
             navDrawOnCanvas.setOnClickListener { navigateToActivity(DrawCanvasActivity::class) }
+            navTodoApp.setOnClickListener { navigateToActivity(TasksActivity::class) }
         }
     }
 }
---
+++ app/src/main/java/com/rowicka/newthings/todoapp/addedittask/AddEditTaskFragment.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.rowicka.newthings.todoapp.addedittask
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
+import com.rowicka.newthings.R
+import com.rowicka.newthings.databinding.FragmentAddtaskBinding
+import com.rowicka.newthings.todoapp.EventObserver
+import com.rowicka.newthings.todoapp.tasks.ADD_EDIT_RESULT_OK
+import com.rowicka.newthings.todoapp.util.setupRefreshLayout
+import com.rowicka.newthings.todoapp.util.setupSnackbar
+
+/**
+ * Main UI for the add task screen. Users can enter a task title and description.
+ */
+class AddEditTaskFragment : Fragment() {
+
+    private lateinit var viewDataBinding: FragmentAddtaskBinding
+
+    private val args: AddEditTaskFragmentArgs by navArgs()
+
+    private val viewModel by viewModels<AddEditTaskViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_addtask, container, false)
+        viewDataBinding = FragmentAddtaskBinding.bind(root).apply {
+            this.viewmodel = viewModel
+        }
+        // Set the lifecycle owner to the lifecycle of the view
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+        return viewDataBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupSnackbar()
+        setupNavigation()
+        this.setupRefreshLayout(viewDataBinding.refreshLayout)
+        viewModel.start(args.taskId)
+    }
+
+    private fun setupSnackbar() {
+        view?.setupSnackbar(this, viewModel.snackbarText, Snackbar.LENGTH_SHORT)
+    }
+
+    private fun setupNavigation() {
+        viewModel.taskUpdatedEvent.observe(viewLifecycleOwner, EventObserver {
+            val action = AddEditTaskFragmentDirections
+                .actionAddEditTaskFragmentToTasksFragment(ADD_EDIT_RESULT_OK)
+            findNavController().navigate(action)
+        })
+    }
+}
