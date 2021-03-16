0d6860fbe50c38d94eed1f9b6e0f7e17d717de3c
RowickaM <rowickam.23@gmail.com>
2021-03-16 12:45:16 +0100

    pause and resume coroutine job in test

--- app/src/test/java/com/rowicka/newthings/todoapp/statistics/StatisticsViewModelTest.kt
+++ app/src/test/java/com/rowicka/newthings/todoapp/statistics/StatisticsViewModelTest.kt
@@ -2,11 +2,15 @@ package com.rowicka.newthings.todoapp.statistics
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.rowicka.newthings.MainCoroutineRule
+import com.rowicka.newthings.getOrAwaitValue
 import com.rowicka.newthings.todoapp.data.source.FakeTestRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class StatisticsViewModelTest{
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
@@ -24,5 +28,16 @@ class StatisticsViewModelTest{
         tasksRepository = FakeTestRepository()
 
         viewModel = StatisticsViewModel(tasksRepository)
+    }
+
+    @Test
+    fun loadTasks_loading(){
+        mainCoroutineRule.pauseDispatcher()
+        viewModel.refresh()
+
+        assertEquals(true, viewModel.dataLoading.getOrAwaitValue())
+        mainCoroutineRule.resumeDispatcher()
+
+        assertEquals(false, viewModel.dataLoading.getOrAwaitValue())
     }
 }
