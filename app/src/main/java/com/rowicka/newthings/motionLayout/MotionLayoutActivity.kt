6c7c70276de359b40b6362be8ecc72aa1191ee29
Magdalena Rowicka <mrowicka@corelogic.pl>
2021-08-18 08:59:37 +0200

    setup

---
+++ app/src/main/java/com/rowicka/newthings/motionLayout/MotionLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.rowicka.newthings.motionLayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rowicka.newthings.R
+
+class MotionLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion_layout)
+    }
+}
